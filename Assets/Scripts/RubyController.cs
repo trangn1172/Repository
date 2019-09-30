01cfeff39246a8a9a3e3257af0ff0de55d34f987
Trang Nguyen <trangn1172@gmail.com>
2019-09-30 14:51:16 -0500

    Ruby Controller Script
    Added more lines of code to the script

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    [Tooltip("Health vaule between 0 and 100")]
+    int health = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-
     }
-
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 0.3f * horizontal * Time.deltaTime;
+        position.y = position.y + 0.3f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
